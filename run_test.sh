3f35ad6f82600c4fdba71b59b3f40d9956f90535
bamtercelboo <bamtercelboo@163.com>
2019-02-24 18:40:55 +0800

    sh file

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,6 @@
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 # device [cpu, cuda0, cuda1 ......]
-nohup python -u main.py --config ./Config/config.cfg --device cuda:1 --t_data test --test > log_test 2>&1 &
+nohup python -u main.py --config ./Config/config.cfg --device cuda:0 --t_data test --test > log_test 2>&1 &
 tail -f log_test
  
-
-
--- run_train_p.sh
+++ run_train_p.sh
@@ -1,8 +1,5 @@
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 # device [cpu, cuda0, cuda1 ......]
-nohup python -u main.py --config ./Config/config.cfg --device cuda:1 --train -p > log_train 2>&1 &
+nohup python -u main.py --config ./Config/config.cfg --device cuda:0 --train -p > log_train 2>&1 &
 tail -f log_train
- 
-
-
